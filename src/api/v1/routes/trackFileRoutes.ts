184cccad049326869d5d6ba94cc7ea13b2882e8d
Seth <sboyer2@academic.rrc.ca>
2026-04-24 12:19:06 -0500

    Add track file docs

--- src/api/v1/routes/trackFileRoutes.ts
+++ src/api/v1/routes/trackFileRoutes.ts
@@ -6,7 +6,39 @@ import isAuthorized from "../middleware/authorize";
 
 const trackFileRouter: express.Router = express.Router();
 
-
+/**
+ * @openapi
+ * /tracks:
+ *   post:
+ *     summary: Create a new track
+ *     tags: [Tracks]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - req.file
+ *             properties:
+ *               req.file:
+ *                   type: audio file
+ *                   description: audio file to be uploaded
+ *                   example: "song.mp3"
+ *     responses:
+ *       '201':
+ *         description: File uploaded successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Item'
+ *       '400':
+ *         description: Invalid input data
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Error'
+ */
 trackFileRouter.post(
   "/tracks/audio", authenticate,
   isAuthorized({ hasRole: ["admin", "manager"] }),
